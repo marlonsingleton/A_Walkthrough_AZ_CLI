b401796acd4a634103c0210123c84631d5902834
Marlon Singleton <marlonosingleton@yahoo.com>
2019-06-29 14:37:29 +0300

    tracking preview commands

---
+++ commands_in_preview.sh
@@ -0,0 +1,20 @@
+# As of 29 June 2019
+
+marlon@localhost:~$ az -h | grep Preview
+    ams               [Preview] : Manage Azure Media Services resources.
+    backup            [Preview] : Manage Azure Backups.
+    bot               [Preview] : Manage Microsoft Azure Bot Service.
+    cache             [Preview] : Commands to manage CLI objects cached using the
+    consumption       [Preview] : Manage consumption of Azure resources.
+    deploymentmanager [Preview] : Create and manage rollouts for your service.
+    dla               [Preview] : Manage Data Lake Analytics accounts, jobs, and catalogs.
+    dls               [Preview] : Manage Data Lake Store accounts and filesystems.
+    hdinsight         [Preview] : Manage HDInsight resources.
+    lab               [Preview] : Manage Azure DevTest Labs.
+    reservations      [Preview] : Manage Azure Reservations.
+    search            [Preview] : Manage Azure Search services, admin keys and query keys.
+    security          [Preview] : Manage your security posture with Azure Security Center.
+    sf                [Preview] : Manage and administer Azure Service Fabric clusters.
+    find              [Preview] : I'm an AI robot, my advice is based on our Azure
+    interactive       [Preview] : Start interactive mode. Installs the Interactive
+    rest              [Preview] : Invoke a custom request.
